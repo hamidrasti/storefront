655ef871d17b3fd3d0be32c13f4f313f3476fe98
hamraa <raasti.hamid@gmail.com>
2022-01-15 23:15:30 +0330

    install django debug toolbar

--- storefront/urls.py
+++ storefront/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
